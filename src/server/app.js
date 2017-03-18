5f55702107208c8c400f3395cbb98f605676f36b
GalBenEvgi <galevgi@gmail.com>
2017-03-18 13:06:20 +0200

    redirect to home page fixed,Karma testing script added (ng test)

--- src/server/app.js
+++ src/server/app.js
@@ -79,7 +79,7 @@ db.once('open', function() {
 
   // all other routes are handled by Angular
   app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname,'/../../dist/index.html'));
+    res.sendFile(path.join(__dirname,'/../../src/index.html'));
   });
 
 
